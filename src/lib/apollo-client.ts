330fa16d544bee4d2f7bfb7b91d51e2c0ee2dae6
Michele Cheow <ldnallen@gmail.com>
2024-07-26 13:35:06 -0700

    added more error handling for better logs

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,4 +1,10 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
 
 // Create an http link
@@ -9,7 +15,6 @@ const httpLink = createHttpLink({
 // Create a function to set the auth context
 const authLink = setContext((_, { headers }) => {
   // Get the token from the environment variables
-  //const token = process.env.BLUE_BOX_MOVIES_API_TOKEN;
   const token = process.env.NEXT_PUBLIC_BBMOVIES_API_TOKEN;
 
   // Return the headers to the context so httpLink can read them
@@ -21,9 +26,20 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 // Create the Apollo client instance
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  //link: authLink.concat(httpLink),
+  link: from([authLink, errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
