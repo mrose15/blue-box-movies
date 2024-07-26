72cf2b1e8f24b8828b4daf1f8637ed070b8e35f5
Michele Cheow <ldnallen@gmail.com>
2024-07-26 11:34:59 -0700

    import Movie list in to page, added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import MovieList from "../components/MovieList/MovieList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Blue Box Movies</h1>
+      <MovieList />
+    </main>
   );
 }
