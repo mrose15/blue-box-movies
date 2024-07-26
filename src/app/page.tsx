94773352f6cad34a12a0d5b6bc4e37fb78bc289f
Michele Cheow <ldnallen@gmail.com>
2024-07-26 16:21:16 -0700

    added react suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import MovieList from "../components/MovieList/MovieList";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Blue Box Movies</h1>
-      <MovieList />
+      <Suspense fallback={<p>Loading movies...</p>}>
+        <MovieList />
+      </Suspense>
     </main>
   );
 }
