710079b39e27a6b4b90453aecf903951608c1f1c
Michele Cheow <ldnallen@gmail.com>
2024-07-29 13:50:52 -0700

    added pagination types and additional movie prop type

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -1,18 +1,36 @@
-export interface Movie {
-  id: string;
-  title: string;
-  summary: string;
-}
-
+// Pagination types
 export interface Pagination {
   page: number;
   perPage: number;
   totalPages: number;
 }
 
+export interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+}
+
+// Movie types
+export interface Movie {
+  id: string;
+  title: string;
+  summary: string;
+}
+
 export interface MoviesData {
-  movies: {
-    nodes: Movie[];
-    pagination: Pagination;
+  nodes: Movie[];
+  pagination: {
+    page: number;
+    perPage: number;
+    totalPages: number;
   };
+}
+
+export interface MovieListProps {
+  page: number;
+  perPage: number;
+}
+
+export interface MoviePageProps {
+  searchParams: { page?: string };
 }
