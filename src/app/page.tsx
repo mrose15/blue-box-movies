1417632c055fa160ea4aa279152059a8d1fe0310
Michele Cheow <ldnallen@gmail.com>
2024-07-29 13:52:15 -0700

    imported MoviePageProps, passed props to function and MovieList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import MovieList from "../components/MovieList/MovieList";
+import { MoviePageProps } from "../types/movies";
 import { Suspense } from "react";
 
-export default function Home() {
+export default function Home({ searchParams }: MoviePageProps) {
+  const page = Number(searchParams.page) || 1;
+  const perPage = 10;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Blue Box Movies</h1>
       <Suspense fallback={<p>Loading movies...</p>}>
-        <MovieList />
+        <MovieList page={page} perPage={perPage} />
       </Suspense>
     </main>
   );
